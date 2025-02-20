774a08793642a4f8cf6d4f5c5017278f5e0d25ff
Eric TRAN-KY <erictranky@gmail.com>
2025-02-20 14:50:00 +0100

    changement root

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "users#index"
+  root "products#index"
 
 end
